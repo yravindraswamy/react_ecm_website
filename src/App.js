1410dbc59b25d8c974850b045728a7fdbeaad108
root <yravindraswamy@gmail.com>
2023-01-22 14:55:37 +0000

    this is the updated code I am getting cors error while I try to get products from server

--- src/App.js
+++ src/App.js
@@ -7,14 +7,15 @@ import Cart from './components/Cart'
 import NotFound from './components/NotFound'
 
 import './App.css'
+import ProtectedRoute from './components/ProtectedRoute'
 
 const App = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/login" component={LoginForm} />
-      <Route exact path="/" component={Home} />
-      <Route exact path="/products" component={Products} />
-      <Route exact path="/cart" component={Cart} />
+      <ProtectedRoute exact path="/" component={Home} />
+      <ProtectedRoute exact path="/products" component={Products} />
+      <ProtectedRoute exact path="/cart" component={Cart} />
       <Route component={NotFound} />
     </Switch>
   </BrowserRouter>
--- src/components/AllProductsSection/index.js
+++ src/components/AllProductsSection/index.js
@@ -1,11 +1,29 @@
 import {Component} from 'react'
-
+import Cookies from 'js-cookie'
 import ProductCard from '../ProductCard'
 import './index.css'
 
 class AllProductsSection extends Component {
   state = {
     productsList: [],
+  }
+
+  componentDidMount() {
+    this.getProductsListFromApi()
+  }
+
+  getProductsListFromApi = async () => {
+    const jwtToken = Cookies.get('jwt_token')
+    const options = {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+      },
+    }
+    const productsUrl = 'http://apis.ccbp.in/products'
+    const response = await fetch(productsUrl, options)
+    const jsonData = await response.json()
+    console.log(jsonData)
   }
 
   renderProductsList = () => {
